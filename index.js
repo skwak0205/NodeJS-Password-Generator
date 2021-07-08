eea68f32e7530be11e53172cc4d558f2d7fb1d68
Soojung Kwak <gsj2464@gmail.com>
2021-07-09 08:49:58 +0900

    created commands & opts

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // commander chalk clipboardy modules 설치
 const program = require('commander');
 
-program.version('1.0.0').description('Simple Password Generator').parse();
+program.version('1.0.0').description('Simple Password Generator');
+
+program
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-s, --save', 'save password to password.txt')
+  .option('-nn, --no-numbers', 'remove numbers')
+  .option('-ns, --no-symbols', 'remove symbols')
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
