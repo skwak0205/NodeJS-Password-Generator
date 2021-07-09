46b06551c9c1ea41a5612890d2104b881d4d659a
Soojung Kwak <gsj2464@gmail.com>
2021-07-09 09:07:49 +0900

    save password to file 기능 추가

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // commander chalk clipboardy modules 설치
 const program = require('commander');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
 const createPassword = require('./utils/createPassword');
+const savePassword = require('./utils/savePassword');
 const log = console.log;
 
 program.version('1.0.0').description('Simple Password Generator');
 
 program
   .option('-l, --length <number>', 'length of password', '8')
-  .option('-s, --save', 'save password to password.txt')
+  .option('-s, --save', 'save password to passwords.txt')
   .option('-nn, --no-numbers', 'remove numbers')
   .option('-ns, --no-symbols', 'remove symbols')
   .parse();
@@ -17,5 +20,14 @@ const { length, save, numbers, symbols } = program.opts();
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols);
 
+// Save to file
+if (save) {
+  savePassword(generatedPassword);
+}
+
+// Copy to clipboard
+clipboardy.writeSync(generatedPassword);
+
 // Output generated password
-log(generatedPassword);
+log(chalk.blue('Generated Password : ') + chalk.bold(generatedPassword));
+log(chalk.yellow('Password copied to clipboard'));
